93b33c86862176b616525f92d398b39a351778e2
Tarvitz <tarvitz@blacklibrary.ru>
2019-04-23 23:24:30 +0300

    [py37] add python3.7 support for distribution

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    # 'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development :: Libraries',
